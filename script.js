afeaa45fe3a51b6e7b0f0efce05c40bd895d00a1
narendar <narendar@gmail.com>
2025-07-24 18:59:33 +0530

    done

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 let pixels=16;
+let container=document.querySelector(".container");
 createSheet(pixels);
 let btn=document.querySelector(".btn");
 let input=document.querySelector('input');
-btn.addEventListener("click",function(){
+
+btn.addEventListener("click",function(e){
+   e.preventDefault();
+   container.innerHTML="";
    pixels =parseInt(input.value);
+   if(pixels>100){
+      pixels=100;
+   }else if(pixels<=0){
+      pixels=16;
+   }
    createSheet(pixels);
 })
 
 function createSheet(pixels){
-   let container=document.querySelector(".container");
       for(let i=0;i<pixels;i++){
          let row=document.createElement("div");
          row.setAttribute("class",`row`);
@@ -18,7 +26,10 @@ function createSheet(pixels){
             box.setAttribute("class","box");
             row.appendChild(box);
             box.addEventListener("mouseover",function(){
-               box.classList.add(".hovered");
+               box.classList.add("hovered");
+            })
+            box.addEventListener("touchstart",function(){
+               box.classList.add("hovered");
             })
          }
       }
